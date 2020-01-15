5ba5e75a96c173e43c9d6df0690f4a63880ba873
Luis Segoviano <luissegoviano.14@gmail.com>
2020-01-14 18:55:59 -0600

    Initial Commit

---
+++ Do Try Catch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Do Try Catch
+//
+//  Created by Luis Segoviano on 14/01/20.
+//  Copyright © 2020 The Segoviano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
