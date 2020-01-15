b2bd363a55541ae957d097933b226f56927af582
Luis Segoviano <luissegoviano.14@gmail.com>
2020-01-14 19:16:39 -0600

    Adding remote origin

--- Do Try Catch/ViewController.swift
+++ Do Try Catch/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        print("fffhfg")
     }
 
 
